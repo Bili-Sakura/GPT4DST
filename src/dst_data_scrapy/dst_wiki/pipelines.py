9b972d94a57c23e60150aedb954568eb7492a9b1
IcyFeather <mengzhuo.happy@gmail.com>
2024-03-13 18:23:57 +0800

    添加了筛选Response URL的pipeline

--- src/dst_data_scrapy/dst_wiki/pipelines.py
+++ src/dst_data_scrapy/dst_wiki/pipelines.py
@@ -6,8 +6,19 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from scrapy.exceptions import DropItem
 
 
 class DstWikiPipeline:
     def process_item(self, item, spider):
         return item
+
+
+class FandomPipeline(object):
+    def process_item(self, item, spider):       
+        if any(discard in item['source'] for discard in
+                   ['?', 'File:', 'Template:', 'Talk:', '#', 'Special:', 'Message_Wall:', 'User:', 'Help:',
+                    'User_blog:', 'Board_Thread:', 'Thread:']):
+            raise DropItem("Drop %s" % item)
+        else:
+            return item
